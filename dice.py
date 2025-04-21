cb0541d0e230867e00b20390ac9453044662d28e
j23447 山本雅也 <j23447ym@edu.tuis.ac.jp>
2025-04-21 10:24:22 +0900

    add
    プログラム完成
    Q-3未完

---
+++ dice.py
@@ -0,0 +1,28 @@
+import random
+
+add=0
+ave=0
+count=0
+for i in range(10):
+    x = random.randint(1,6)
+    print(str(i+1)+"回目："+str(x))
+    add=add+x
+    count+=1
+
+ave=add/count
+print("平均値：" + str(ave))
+
+# 期待される出力結果例
+"""
+1回目：4
+2回目：1
+3回目：1
+4回目：5
+5回目：1
+6回目：1
+7回目：5
+8回目：6
+9回目：4
+10回目：6
+平均値：3.4
+"""
